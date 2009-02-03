0455fe34c24aee709c2575e384208dbee278ad1a
godfat <godfat@godfat.org>
2009-02-03 22:18:47 +0800

    require all models in model/init.

--- model/init.rb
+++ model/init.rb
@@ -2,3 +2,16 @@
 
 # Here go your requires for models:
 # require 'model/user'
+
+require 'rubygems'
+require 'dm-core'
+
+DataMapper.setup(:default, JinRou.yaml_load('config/database.yaml')['uri'])
+
+Dir.glob(File.dirname(__FILE__) + "/*.rb").map{ |file|
+  # snip base dir and file ext
+  file = file[ (file =~ %r{model/})..-1 ][0..-4]
+
+  next if file == 'model/init'
+  require file
+}
