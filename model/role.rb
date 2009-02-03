37b169183d2362d1bfbb3ee7dfbfc4a7a7132972
godfat <godfat@godfat.org>
2009-02-03 22:19:11 +0800

    first role draft.

---
+++ model/role.rb
@@ -0,0 +1,28 @@
+
+class Role
+  include DataMapper::Resource
+  property :id, Serial
+  property :type, Discriminator
+  property :name, String
+end
+
+class Villager < Role
+end
+
+class Astrologer < Role
+end
+
+class Werewolf < Role
+end
+
+class Coroner < Role
+end
+
+class Guardian < Role
+end
+
+class Disciple < Role
+end
+
+class HuliJin < Role
+end
