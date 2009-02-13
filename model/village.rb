320ff8a6d0d22388104022a8abe9f96b591fe421
godfat <godfat@godfat.org>
2009-02-13 21:11:52 +0800

    added Village#dispatch_player to dispatch role and character.

--- model/village.rb
+++ model/village.rb
@@ -8,4 +8,27 @@ class Village
 
   has n, :players
   has n, :days
+
+  def dispatch_player
+    chars   = Character.all.shuffle
+    roles   =      Role.all.map{ |role|
+                     [role] * case role
+                                when Villager;   10
+                                when Astrologer;  1
+                                when Coroner;     1
+                                when Guardian;    1
+                                when Disciple;    2
+                                when Avenger;     1
+                                when Werewolf;    4
+                                when Tobewolf;    1
+                                when HuliJin;     1
+                              end
+                   }.flatten.shuffle
+
+    players.each{ |pl|
+      pl.update_attributes( :character => chars.pop,
+                                 :role => roles.pop  )
+    }
+
+  end
 end
