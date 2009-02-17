b13a28d32e44f73d6ecb298a29bee59a6534ee0d
godfat <godfat@godfat.org>
2009-02-17 21:04:10 +0800

    added index for turn. 0: not started. odd: night. even: day.

--- model/turn.rb
+++ model/turn.rb
@@ -4,6 +4,11 @@ class Turn
   property :id, Serial
   timestamps :at
 
+  # 0    => waiting room
+  # odd  => night
+  # even => day
+  property :index, Integer
+
   belongs_to :game
 
   has n, :messages
